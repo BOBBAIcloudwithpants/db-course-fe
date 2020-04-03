57866dd5190fbc8faff7599239cf5175dd1d687d
bob <bobbaicloudwithpants@gmail.com>
2020-04-03 08:49:26 +0800

    登录功能

--- dbse/login/models.py
+++ dbse/login/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
-    user_id = models.IntegerField(primary_key=True,auto_created=True)
+    user_id = models.IntegerField(primary_key=True, auto_created=True)
     username = models.CharField(max_length=40)
     password = models.CharField(max_length=40)
+
     class Meta:
-        db_table='user'
+        db_table = 'user'
+
+    def createUser(self):
+        print(self.password)
+        self.findUser()
+        sql = "insert into user(username, password) values ('%s','%s')" % (self.username, self.password)
+        print(sql)
+        User.objects.raw(sql)
+        print("插入用户: {} 密码: {}, 成功".format(self.username, self.password))
+        return True
+
+    def findUser(self):
+        sql = "select * from user where username='%s'" % (self.username)
+
+
+
--- dbse/login/views.py
+++ dbse/login/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+from . import models
 
-# Create your views here.
+
+def hello(request):
+    return JsonResponse({'result': 200, 'msg': '连接成功'})
+
+
+def register(request):
+    req = json.loads(request.body)
+
+    user = models.User(0, req['username'], req['password'])
+    if models.User.createUser(user):
+        return JsonResponse({'result': 200, 'msg': '创建用户成功'})
+    else:
+        return JsonResponse({'result': 500, 'msg': '创建用户失败'})
+
+
+
